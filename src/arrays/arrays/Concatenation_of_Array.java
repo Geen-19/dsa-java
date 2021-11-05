6449d0d9aab09251dcb162c22a0ab0516e44e7e5
Geen Donald Joel <91582806+Geen-19@users.noreply.github.com>
2021-11-05 10:52:46 +0530

    Solution for Concatenation Of Arrays

---
+++ src/arrays/arrays/Concatenation_of_Array.java
@@ -0,0 +1,22 @@
+package arrays.arrays;
+
+public class Concatenation_of_Array {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2*nums.length];
+        int[] sum1 = new int[nums.length];
+        int n = nums.length;
+
+
+        for (int i = 0; i < 2*nums.length; i++) {
+            if(i< nums.length){
+                ans[i] = nums[i];
+            }
+            if(i>= nums.length){
+                ans[i] = nums[i-n];
+            }
+
+        }
+        return ans;
+
+    }
+}

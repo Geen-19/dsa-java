bbac244f8934900c6697e878facec73b8b13594b
Geen Donald Joel <91582806+Geen-19@users.noreply.github.com>
2021-11-07 11:14:05 +0530

    Solution for One Plus
    This is done by changing the sum to long

---
+++ src/arrays/arrays/One_Plus.java
@@ -0,0 +1,33 @@
+package arrays.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class One_Plus {
+    public static long[] addToArrayForm(int[] nums) {
+    long sum = 0;
+    long rem;
+    int counter = 0;
+    long k =1;
+        for (int i = 0; i < nums.length; i++) {
+        rem = nums[i];
+        sum = sum*10 + rem;
+    }
+    List<Integer> fun = new ArrayList<>();
+    long fin = sum + k;
+    long gna = fin;
+        while(fin>0){
+        fin = fin/10;
+        counter = counter +1;
+    }
+    long[] reece = new long[counter];
+    int[] ans = new int[counter];
+    int help = counter-1;
+        while (help>=0 ) {
+        rem = gna % 10;
+        reece[help] = rem ;
+        help--;
+        gna=gna/10;
+    }
+        return reece;
+}
